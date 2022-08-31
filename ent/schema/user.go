c46fff865f6aebbec9e620c6d1254b349ff0bba7
Muhammed-Abdo-20403 <mhdshaikh20403@gmail.com>
2022-08-31 17:06:07 +0200

    added fields to user entity

--- ent/schema/user.go
+++ ent/schema/user.go
@@ -1,6 +1,11 @@
 package schema
 
-import "entgo.io/ent"
+import (
+	"time"
+
+	"entgo.io/ent"
+	"entgo.io/ent/schema/field"
+)
 
 // User holds the schema definition for the User entity.
 type User struct {
@@ -9,7 +14,19 @@ type User struct {
 
 // Fields of the User.
 func (User) Fields() []ent.Field {
-	return nil
+	return []ent.Field{
+		field.String("name").MaxLen(70),
+		field.String("channel_name").MaxLen(70).Unique(),
+		field.String("email").Unique(),
+		field.String("specialist").Optional(),
+		field.Int("age").Optional().Positive(),
+		field.String("phone").Optional(),
+		field.String("language").Optional(),
+		field.String("country"),
+		field.Text("shor_bio").MaxLen(270),
+		field.Time("created_at").Default(time.Now).Immutable(),
+		field.Time("updated_at").Default(time.Now).UpdateDefault(time.Now),
+	}
 }
 
 // Edges of the User.

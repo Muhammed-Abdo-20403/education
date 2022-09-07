a15a26dd42b3c77e799f43058b1f1b86234658d1
Muhammed-Abdo-20403 <mhdshaikh20403@gmail.com>
2022-09-07 17:21:58 +0200

    add EditUser, GetUser and  DeleteUser

--- api/user_controller/user.go
+++ api/user_controller/user.go
@@ -3,8 +3,10 @@ package userController
 import (
 	"education/config"
 	"education/ent"
+	"education/ent/user"
 	"fmt"
 	"net/http"
+	"strconv"
 
 	"github.com/gin-gonic/gin"
 )
@@ -51,13 +53,56 @@ func CreateUsers(c *gin.Context) {
 }
 
 func EditUser(c *gin.Context) {
+	var inputs *ent.User
 
+	if err := c.ShouldBindJSON(&inputs); err != nil {
+		c.AbortWithStatusJSON(http.StatusBadRequest, err.Error())
+		return
+	}
+
+	userID, _ := strconv.Atoi(c.Param("id"))
+
+	user, err := config.Client.User.
+		UpdateOneID(userID).
+		SetName(inputs.Name).
+		SetEmail(inputs.Email).
+		Save(c)
+	if err != nil {
+		c.AbortWithStatusJSON(http.StatusBadRequest, err.Error())
+	}
+
+	c.JSON(http.StatusOK, user)
 }
 
 func GetUser(c *gin.Context) {
+	userID, err := strconv.Atoi(c.Param("id"))
 
+	if err != nil {
+		c.AbortWithStatusJSON(http.StatusBadRequest, err.Error())
+	}
+
+	user, err := config.Client.User.
+		Query().
+		Where(user.ID(userID)).
+		First(c)
+
+	if err != nil {
+		fmt.Println(err.Error())
+		c.AbortWithStatus(http.StatusBadRequest)
+	}
+
+	c.JSON(http.StatusOK, user)
 }
 
 func DeleteUser(c *gin.Context) {
+	userID, err := strconv.Atoi(c.Param("id"))
 
+	if err != nil {
+		c.AbortWithStatusJSON(http.StatusBadRequest, err.Error())
+		return
+	}
+
+	user := config.Client.User.DeleteOneID(userID).Exec(c)
+
+	c.JSON(http.StatusCreated, user)
 }

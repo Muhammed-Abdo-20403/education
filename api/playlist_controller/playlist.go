838b216bdadd5e31b6ee320f1d094b06aae1497e
Muhammed-Abdo-20403 <mhdshaikh20403@gmail.com>
2022-09-07 17:29:53 +0200

    add funcs controller

--- api/playlist_controller/playlist.go
+++ api/playlist_controller/playlist.go
@@ -1,1 +1,23 @@
 package playlistController
+
+import "github.com/gin-gonic/gin"
+
+func GetPlaylists(c *gin.Context) {
+
+}
+
+func CreatePlaylist(c *gin.Context) {
+
+}
+
+func EditPlaylist(c *gin.Context) {
+
+}
+
+func GetPlaylist(c *gin.Context) {
+
+}
+
+func DeletePlaylist(c *gin.Context) {
+
+}
